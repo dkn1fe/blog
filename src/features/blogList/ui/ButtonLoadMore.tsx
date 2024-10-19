a99f16bfc49fe873492d533b73f597119fbd64f2
dkn1fe <dknifeb@mail.ru>
2024-10-19 21:11:07 +0300

    change style in button load more

--- src/features/blogList/ui/ButtonLoadMore.tsx
+++ src/features/blogList/ui/ButtonLoadMore.tsx
@@ -3,7 +3,7 @@
 export const ButtonLoadMore = () => {
     return (
        <div className='flex justify-center my-8'>
-           <button className='border border-[#696A75] text-[#696A75] rounded-lg px-3 py-2'>
+           <button className='border border-[#696A75]/[0.50] text-[#696A75] rounded-lg px-3 py-2'>
                Load More
            </button>
        </div>
